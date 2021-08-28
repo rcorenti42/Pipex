a7f415c3de7933193db11e67a055d26fbded989f
Richard CORENTIN <rcorenti@e1r12p16.42.fr>
2021-08-28 07:52:55 +0200

    split and execve

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -4,7 +4,7 @@ void	analyse()
 {
 }
 
-void	parent_process(int f2, int cmd2)
+void	parent_process(int f2, int cmd2, char **mypaths, char **mycmdargs)
 {
 	int	status;
 
@@ -16,29 +16,42 @@ void	parent_process(int f2, int cmd2)
 	close(f2);
 }
 
-void	child_process(int f1,int cmd1)
+void	child_process(int f1,int cmd1, char **mypaths, char **mycmdargs)
 {
+	int		i;
+	char	*cmd;
+
+	i = -1;
 	if (dup2() < 0)
 		exit();
 	dup2(f1, STDIN_FILENO);
 	dup2(end[1], STDOUT_FILENO);
+	while (mypaths[++i])
+	{
+		cmd = ft_strjoin(mypaths[i], argv[2]);
+		execve(cmd, mycmdargs, envp);
+	}
 	close(end[0]);
 	close(f1);
 }
 
 void	pipex(int f1, int f2, char **argv, char **envp)
 {
-	int	end[2];
-	t_pid parent;
+	int		end[2];
+	char	**mypaths;
+	char	**mycmdargs;
+	t_pid	parent;
 
+	mypaths = ft_split(envp, ":");
+	mycmdargs = ft_split(argv[2], " ");
 	pipe(end);
 	parent = fork();
 	if (parent < 0)
 		return (perror("Fork: "));
 	if (!parent)
-		child_process(f1, cmd1);
+		child_process(f1, cmd1, mypaths, mycmdargs);
 	else
-		parent_process(f2, cmd2);
+		parent_process(f2, cmd2, mypaths, mycmdargs);
 }
 
 int		main(int argc, char **argv, char **envp)
